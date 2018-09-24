4aafb9d1f8cf70d120875c83b73088bd438072b9
Matt Gabor <mgabor@apple.com>
2018-09-23 19:44:14 -0700

    added styled components inner

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
-export default ({ children }) => (
-  <div>
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+export const Page = ({ children }) => (
+  <StyledPage>
     <Meta />
     <Header />
-    {children}
-  </div>
+    <Inner>{children}</Inner>
+  </StyledPage>
 );
+
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Page;
