152a08b5fea8a6857c8b116968f0ff7e2286960c
Matt Gabor <mgabor@apple.com>
2018-11-23 16:24:46 -0800

    completed episode 12

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,9 @@
+// This file connects us to the remote prisma DB and gives the ability to query it with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
